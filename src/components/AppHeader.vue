d605d65b6308b592a0479161daa61450054e49ff
Pietroarmill <pietroarmillotta38@gmail.com>
2022-05-19 16:23:59 +0200

    add  AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="header">
+    <div class="header__img">
+      <img src="../assets/img/logo-small.svg" alt="">
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/common.scss";
+
+.header {
+  background-color: #2e3a46;
+  padding: 1rem;
+  &__img {
+    width: 50px;
+  }
+}
+</style>
